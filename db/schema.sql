94f79930d3500e9ac21395d9fc28234b4c8b3b24
Steve Kavuu <mrkavuu@gmail.com>
2020-10-29 22:01:03 -0500

    cleaning

--- db/schema.sql
+++ db/schema.sql
@@ -5,19 +5,16 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 
-INSERT INTO department (departmentName) VALUES ("HR");
-INSERT INTO department (departmentName) VALUES ("IT");
-INSERT INTO department (departmentName) VALUES ("Legal");
 
 INSERT INTO department (departmentName) VALUES ('HEY');
-INSERT INTO department (departmentName) VALUES ("Media");
+
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     departmentName VARCHAR(30),
     PRIMARY KEY (id)
 );
-INSERT INTO roles (title, salary, department_id) VALUES ('Back End', 10000, 1);
+
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
@@ -27,7 +24,7 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY(department_id) REFERENCES department (id)
 );
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Steve', 'Mcsteverson', 1);
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL, 
@@ -40,4 +37,4 @@ CREATE TABLE employee (
 
 SELECT * FROM roles;
 SELECT * FROM department; 
-SELECT * FROM employee; 
+SELECT * FROM employee JOIN roles ON employee.role_id = roles.id;  
